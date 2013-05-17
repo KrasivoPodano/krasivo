a758c723fbc2dd8d88f0b0449be0a570303e73b2
Viacheslav Bobrov <babrovka@gmail.com>
2013-05-17 18:58:53 +0400

    deploy recipe update

--- config/deploy.rb
+++ config/deploy.rb
@@ -88,6 +88,18 @@ end
 
   set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use #{rvm_ruby_string} do bundle exec unicorn_rails -Dc #{unicorn_conf})"
 
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+end
 
 # - for unicorn - #
 namespace :deploy do
