9423bd6733423a9c17e3da4b82553752d8140982
Viacheslav Bobrov <babrovka@gmail.com>
2013-05-27 20:10:06 +0400

    fix users table error

--- app/admin/appointments.rb
+++ app/admin/appointments.rb
@@ -1,17 +1,3 @@
 ActiveAdmin.register Appointment do
-  config.batch_actions = false
-  filter :user, :collection => User.all.map(&:email)
-  filter :event
-  
-  
-  index do 
-    column :user_id do |row|
-      User.find(row.user_id).email
-    end
-    column :event_id do |row|
-      event = Event.find(row.event_id)
-      link_to event.title, event
-    end
-    default_actions
-  end
+
 end
