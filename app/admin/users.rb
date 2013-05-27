f795777093f852118b31a55c2b0efa353060df83
Viacheslav Bobrov <babrovka@gmail.com>
2013-05-27 20:23:21 +0400

    user model back-end

---
+++ app/admin/users.rb
@@ -0,0 +1,30 @@
+ActiveAdmin.register User do
+    config.batch_actions = false
+  
+  index do                            
+    column :email                     
+      
+    default_actions                   
+  end                                 
+
+  filter :email                       
+
+  form do |f|                         
+    f.inputs "Admin Details" do     
+      f.input :name  
+      f.input :email                  
+      f.input :password               
+      f.input :password_confirmation  
+    end                               
+    f.actions                         
+  end
+  
+  show do
+    
+    attributes_table do
+      row :name
+      row :email
+    end
+  end
+  
+end
