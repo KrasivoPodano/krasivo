2150be6bed955b75f0d642400c5b5860cb5105cd
Viacheslav Bobrov <babrovka@gmail.com>
2013-06-03 11:26:23 +0400

    orders migration fix

--- db/migrate/20130602202835_change_event_type.rb
+++ db/migrate/20130602202835_change_event_type.rb
@@ -1,7 +1,12 @@
 class ChangeEventType < ActiveRecord::Migration
-  def change
+  def up
     remove_column :orders, :event_type_id
     add_column :orders, :event_type, :string
+  end
+  
+  def down
+    add_column :orders, :event_type_id, :integer
+    remove_column :orders, :event_type
   end
 
 end
