a46f4bf946ba4855908f185131287037cfa74cbe
Viacheslav Bobrov <babrovka@gmail.com>
2013-06-04 21:10:24 +0400

    event show event limit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < ApplicationController
     unless @event.album.nil?
     @photos = @event.album.photos
     end
-    @events = Event.where("date >= ?", Time.now ).order("date asc")
+    @events = Event.where("date >= ?", Time.now ).order("date asc").limit(3)
   end
   
   def future
