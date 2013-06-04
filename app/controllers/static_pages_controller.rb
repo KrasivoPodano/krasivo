8ce73aff99dc505348f76ab3e5928fd7ded5bf1b
Viacheslav Bobrov <babrovka@gmail.com>
2013-06-04 22:47:13 +0400

    if clause for event show

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @main = Event.main.with_images.all
+    @main = Event.main.all
     @future = Event.where("date >= ?", Time.now ).order("date desc").limit(3)
     @past = Event.where("date <= ?", Time.now ).order("date asc").limit(3)
   end
