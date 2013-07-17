6269229a9ae7d7b4f82c69327a5d2e07fc71eb16
Viacheslav Bobrov <babrovka@gmail.com>
2013-07-17 11:09:01 +0400

    appointment mailer

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -5,11 +5,13 @@ class AppointmentsController < ApplicationController
   end
   
   def create
-    
     @appointment = Appointment.create(params[:appointment])
-    
+    @firstname = params[:appointment][:firstname]
+    @lastname = params[:appointment][:lastname]
+    @phone = params[:appointment][:phone]
     @event = Event.find(params[:appointment][:event_id])
     
+    OrderMailer.appointment_email(@firstname, @lastname, @phone, @event).deliver
 
     
     respond_to do |format|
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -6,11 +6,12 @@ class OrderMailer < ActionMailer::Base
     mail(:to => "info@krasivopodano.ru", :cc => "m.nikolaev@me.com", :bcc => "babrovka@gmail.com", :subject => "krasivopodano.ru | " + t("new_order"))
   end
   
-  def appointment_email(user, event, appointment)
-    @user = user
-    @event = event
-    @appointment = appointment
-    mail(:to => @user.email, :bcc => ["m.nikolaev@me.com", "babrovka@gmail.com"], :subject => "krasivopodano.ru | " + t("appointment_created"))
+  def appointment_email(firstname, lastname, phone, event)
+    @firstname = firstname
+    @lastname = lastname
+    @phone = phone
+    @event = event.title
+    mail(:to => "info@krasivopodano.ru", :bcc => ["m.nikolaev@me.com", "babrovka@gmail.com"], :subject => "krasivopodano.ru | " + t("appointment_created"))
   end
   
   def appointment_confirm_email(user, event, appointment)
