c2ce8a7ccd95cd7cb8db6a29b13239b6fdf1ff2b
Viacheslav Bobrov <babrovka@gmail.com>
2013-07-19 19:50:28 +0400

    facebook

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -6,6 +6,7 @@ ActiveAdmin.register Article do
   
   member_action :crosspost, :method => :post do
     article = Article.find(params[:id])
+    SocialPoster.write(:fb, article.title)
     SocialPoster.write(:twitter, article.title)
     SocialPoster.write(:lj, article.text, article.title)
     redirect_to :action => :show
