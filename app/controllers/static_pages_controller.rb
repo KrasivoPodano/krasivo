16fee7847067acf6a14232f4acee0f16af240e87
Viacheslav Bobrov <babrovka@gmail.com>
2013-07-30 13:35:09 +0400

    published events

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
-    @main = Event.main.future
-    @future = Event.where("date >= ?", Time.now ).order("date asc").limit(3)
-    @past = Event.where("date <= ?", Time.now ).order("date desc").limit(3)
+    @main = Event.published.main.future
+    @future = Event.published.where("date >= ?", Time.now ).order("date asc").limit(3)
+    @past = Event.published.where("date <= ?", Time.now ).order("date desc").limit(3)
   end
   
   def contacts
-    @event = Event.where("date >= ?", Time.now ).order("date asc").first
-    @events = Event.where("date >= ?", Time.now ).order("date asc").limit(3)
+    @event = Event.published.where("date >= ?", Time.now ).order("date asc").first
+    @events = Event.published.where("date >= ?", Time.now ).order("date asc").limit(3)
   end
   
   def shcool
