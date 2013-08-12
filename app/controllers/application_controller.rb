8a48070e26c62c7c74787c674cd085c4d182e6da
Viacheslav Bobrov <babrovka@gmail.com>
2013-08-12 09:58:08 +0400

    login redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-   
-  after_filter :store_location
-
-  def store_location
-    # store last url as long as it isn't a /users path
-    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/ || request.fullpath =~ /\/admin\/login/ || request.fullpath =~ /\/admin/ 
-  end
-
-  def after_sign_in_path_for(resource)
-    session[:previous_url] || root_path
-  end
-
   def current_cart
     Cart.find(session[:cart_id])
   rescue ActiveRecord::RecordNotFound
