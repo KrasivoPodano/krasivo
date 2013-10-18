8b2fcd72bd567a71d47b4356c1d15cca91eab5ef
Viacheslav Bobrov <babrovka@gmail.com>
2013-10-18 16:13:46 +0400

    email css

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -2,8 +2,8 @@ class SubscriptionMailer < ActionMailer::Base
   default :css => 'email', from: "info@krasivopodano.ru"
   
   def events_feed_email
-    @events = Event.master_classes.where(:date => Time.now.next_week..Time.now.next_week.end_of_week).order('date ASC')
-    @courses = Event.courses
+    @events = Event.published.master_classes.where(:date => Time.now.next_week..Time.now.next_week.end_of_week).order('date ASC')
+    @courses = Event.published.courses
     mail(to: ["m.nikolaev@me.com", "babrovka@gmail.com"], subject: t("events_for_the_next_week"))
   end
   
