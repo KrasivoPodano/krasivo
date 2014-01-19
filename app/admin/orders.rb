ce62a918f36a0c173222450333bc6ae372804b3b
Viacheslav Bobrov <babrovka@gmail.com>
2014-01-19 21:04:32 +0400

    orders list

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -6,19 +6,10 @@ ActiveAdmin.register Order do
     column :id
     column :name
     column :phone1
-    column :phone2
-    column :phone3
     column :mail
-    column :comment
-    column :event_id
     column :people
     column :date
     column :time
-    column :beautiful_cake
-    column :ornament
-    column :children_cake
-    column :animators
-    column :money
     column :event_type
     
     default_actions
