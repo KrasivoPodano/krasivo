3c2ead71ab3e0499fbdbb353348a1c40acb7c4c1
babrovka <babrovka@gmail.com>
2014-07-02 15:09:30 +0400

    add label to mails

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,11 @@
+# coding: utf-8
+
 class OrderMailer < ActionMailer::Base
   default :from => "info@krasivopodano.ru"
   
   def confirm_email(order)
     @order = order
-    mail(:to => "info@krasivopodano.ru", :cc => "m.nikolaev@me.com", :bcc => "babrovka@gmail.com", :subject => "krasivopodano.ru | " + t("new_order"))
+    mail(:to => "info@krasivopodano.ru", :cc => "m.nikolaev@me.com", :bcc => "babrovka@gmail.com", :subject => "ВАЖНО: krasivopodano.ru | " + t("new_order"))
   end
   
   def appointment_email(firstname, lastname, phone, event)
@@ -11,13 +13,13 @@ class OrderMailer < ActionMailer::Base
     @lastname = lastname
     @phone = phone
     @event = event
-    mail(:to => "info@krasivopodano.ru", :bcc => ["m.nikolaev@me.com", "babrovka@gmail.com"], :subject => "krasivopodano.ru | " + t("appointment_created"))
+    mail(:to => "info@krasivopodano.ru", :bcc => ["m.nikolaev@me.com", "babrovka@gmail.com"], :subject => "ВАЖНО: krasivopodano.ru | " + t("appointment_created"))
   end
   
   def appointment_confirm_email(user, event, appointment)
     @user = user
     @event = event
     @appointment = appointment
-    mail(:to => @user.email, :bcc => ["m.nikolaev@me.com", "babrovka@gmail.com"], :subject => "krasivopodano.ru | " + t("appointment_confirmed"))
+    mail(:to => @user.email, :bcc => ["m.nikolaev@me.com", "babrovka@gmail.com"], :subject => "ВАЖНО: krasivopodano.ru | " + t("appointment_confirmed"))
   end
 end
