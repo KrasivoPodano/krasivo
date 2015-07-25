c1e4aa5cdae3e9cd005a237091b4de47443703b1
Dragon Black <db87987@gmail.com>
2015-07-25 18:21:17 +0300

    Changes shema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150725122051) do
+ActiveRecord::Schema.define(:version => 20150725133315) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -330,8 +330,9 @@ ActiveRecord::Schema.define(:version => 20150725122051) do
     t.string   "title"
     t.string   "link"
     t.integer  "position"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+    t.integer  "row",        :default => 1, :null => false
   end
 
   create_table "users", :force => true do |t|
